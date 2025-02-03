efd543442a24c2c9a08bb4e09abd08df2918c22a
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-03 16:16:30 +0530

    Create SubStringCheck.java

---
+++ SubStringCheck.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class DigitsOccurence{
+  /*
+   * There may other integers present between 0 and 0,7
+Example:
+Input list : [2,0,5,0,7,3]
+Output: 1 occurrence of 0,0,7
+   */
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements :");
+       
+       
+
+    }
+}
