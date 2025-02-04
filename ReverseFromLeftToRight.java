b3f09a1f5df04e4c36da0a3417ce36d502d9cd53
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-04 23:02:19 +0530

    Create ReverseFromLeftToRight.java

---
+++ ReverseFromLeftToRight.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ReverseFromLeftToRight {
+    public static void Reversed(int[] arr, int k){
+        int n = arr.length;
+        for(int i=0;i<n;i+=k){
+            int left =i;
+            int right= Math.min(i+k-1,n-1);
+            while(left< right){
+                int temp = arr[left];
+                arr[left]= arr[right];
+                arr[right]= temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4,5,6,7,89,12,1,3,4,5,45,46};
+        int  k =arr.length;
+        Reversed(arr, k);
+        System.out.println("Array Reversed from left to right :"+ Arrays.toString(arr));
+    }
+}
