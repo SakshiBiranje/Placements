5d4f3bac584835b1703f31250f1584377395092e
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-05 23:04:57 +0530

    Update DigitsOccurence.java

--- DigitsOccurence.java
+++ DigitsOccurence.java
@@ -7,13 +7,19 @@ Example:
 Input list : [2,0,5,0,7,3]
 Output: 1 occurrence of 0,0,7
    */
-
+// simple optimized solution
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter elements :");
-       
+        List<Integer> li = Arrays.asList(2,0,5,0,7,3);
+        int count =0;
+        for(int i=0;i<li.size()-2;i++){
+            if(li.get(i) ==0 && li.get(i+2)==0 && li.get(i+3)==7){
+                count ++;
+            }
+        }
+       System.out.println(count + " occurrence of 0,0,7 ");
        
 
     }
-    public static int OccurenceCount()
+   
 }
