403c972bd58e8a1b373510b4630533675d2abfcc
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-06 22:32:41 +0530

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,22 +1,21 @@
 public class Main2 {
     public static void main(String[] args) {
-        String input = "a1b2c3"; // Input string
-        String result = modifyString(input); // Modify string
-        System.out.println(result); // Output the result
+        String input = "a1b2c3"; 
+        String result = modifyString(input); 
+        System.out.println(result);
     }
 
     public static String modifyString(String str) {
         StringBuilder modifiedString = new StringBuilder();
 
-        // Iterate through the string
+        
         for (int i = 0; i < str.length(); i++) {
-            // Every 3rd character (1-based indexing)
+            
             if ((i + 1) % 3 == 0) {
-                // Add previous character multiple times
                 char prevChar = str.charAt(i - 1);
-                modifiedString.append(String.valueOf(prevChar).repeat(i)); // Repeat the previous character i times
+                modifiedString.append(String.valueOf(prevChar).repeat(i)); 
             } else {
-                modifiedString.append(str.charAt(i)); // Otherwise, just add the current character
+                modifiedString.append(str.charAt(i)); 
             }
         }
 
