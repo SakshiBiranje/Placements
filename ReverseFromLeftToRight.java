59fb80b9cd84450926b60e1c9957fcd387259068
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-06 22:32:45 +0530

    Update ReverseFromLeftToRight.java

--- ReverseFromLeftToRight.java
+++ ReverseFromLeftToRight.java
@@ -16,7 +16,7 @@ public class ReverseFromLeftToRight {
         }
     }
     public static void main(String[] args) {
-        int[] arr = { 1,2,3,4,5,6,7,89,12,1,3,4,5,45,46};
+        int[] arr = { 1,2,3,4,5,6,7};
         int  k =arr.length;
         Reversed(arr, k);
         System.out.println("Array Reversed from left to right :"+ Arrays.toString(arr));
