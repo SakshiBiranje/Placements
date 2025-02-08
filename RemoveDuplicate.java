9b969865b9ecbd76690ab0890faff6a1fbf3170d
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-02-08 19:55:16 +0530

    Update RemoveDuplicate.java

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -18,7 +18,6 @@ public class RemoveDuplicate {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array:");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
         System.out.println("Enter the elements of the array:");
         for(int i = 0; i <n; i++) {
